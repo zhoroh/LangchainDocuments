4436bccd7315fe0c74c5b83db5cc19db2cb2304a
Oreoluwa <bolarinwaoreoluwa24@gmail.com>
2024-01-21 23:41:39 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,1 @@
-chat_history =[]
-def update_chat_history_and_get_answer(prompt,qa):
-    global chat_history
-    # print("global history is", chat_history)
-    result = qa({"question": prompt, "chat_history": chat_history})
-    response = result['answer']
-    chat_history.append((prompt,response))
-    return response
-
-def clear_history():
-    global chat_history
-    chat_history = []
+#some code was here
